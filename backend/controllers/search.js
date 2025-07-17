aaa59a5617e3573b73744ccf2f1287e30daca58f
Gokulashik M <119474711+Gokulashik22014@users.noreply.github.com>
2025-07-17 23:19:02 +0530

    test3

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -1,7 +1,7 @@
 import { db } from "../sharedValues.js";
-
+import * as wanakana from "wanakana"
 //search a word
-const getData = (key,value) => {
+const getData = (key,value,option) => {
   return new Promise((resolve, reject) => {
     db.find({ [key]: value }, (err, doc) => {
       if (err) {
@@ -14,14 +14,26 @@ const getData = (key,value) => {
     });
   });
 };
+const updateSearchedValue=(word)=>{
+  return new Promise((resolve,reject)=>db.update({word:word},{$set:{isSearched:true}},(err,doc)=>{
+    if(err) return reject({status:false,message:"error"})
+      return resolve({status:true,doc})
+  }))
+}
 export const searchWord = async (req,res) => {
-  const {word}=req.query
-  console.log(word)
-  await getData("word",word)
-    .then((result) =>{
-        if(result.status) res.json({status:true,result:result.result})
+  var {word,isSearched}=req.query
+  if(wanakana.isRomaji(word)){
+    word=wanakana.toHiragana(word)
+  }
+  await getData("word",word,isSearched)
+  .then((result) =>{
+    if(result.status) res.json({status:true,result:result.result})
     })
-    .catch((err) => res.json({staus:false,message:"Not Found"}));
+  .catch((err) => res.json({staus:false,message:"Not Found"}));
+  if(isSearched==="true"){
+    console.log("i run")
+    await updateSearchedValue(word).then((res)=>console.log(res)).catch((err)=>console.log(err))
+  }
 };
 export const getSearchedWord=async(req,res)=>{
     await getData("isSearched",true).then((result) =>{
